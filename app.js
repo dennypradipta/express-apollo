cbeab4ef00cb087aedd32b83c69fa7cbfd5c05c1
Denny <denny@hyperjump.tech>
2020-02-03 11:17:27 +0700

    Feat : Add Apollo Server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const { ApolloServer } = require("apollo-server-express");
 const app = express();
 
 app.use(dotenv.config());
@@ -10,5 +10,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const apollo = new ApolloServer({
+  modules: []
+});
+
+apollo.applyMiddleware({ app });
 
 module.exports = app;
